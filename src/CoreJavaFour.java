12d0624e469ac2e6c94d6f593c28eae688dcc786
rfnshare <rfnshare@gmail.com>
2021-12-12 18:12:24 +0600

    Class, Objects

---
+++ src/CoreJavaFour.java
@@ -0,0 +1,13 @@
+import com.company.Sample;
+
+public class CoreJavaFour {
+    public static void main(String[] args) {
+        CoreJavaFour.hehe();
+        Sample s = new Sample();
+        s.another();
+    }
+    private static void hehe()
+    {
+        System.out.println("hehe");
+    }
+}
