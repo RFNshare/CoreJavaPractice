3924b3fb625d3f90e909bd47770dbfb3505fb428
rfnshare <rfnshare@gmail.com>
2021-12-12 13:58:01 +0600

    Array Declaration

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,17 @@ public class Main {
         double d = 5.99;
         boolean myCard = true;
         System.out.println(myNumber + " " + myString + " " + letter + " " + dec + " " + d + " " + myCard);
+        // Declared an array
+        int[] arr = new int[5];
+        arr[0] = 11;
+        arr[1] = 27;
+        arr[2] = 13;
+        arr[3] = 14;
+        arr[4] = 17;
+
+        int[] arr2 = {1,2,3,4,5};
+        System.out.println(arr[3]);
+        System.out.println(arr2[3]);
 
     }
 }
