86d3fc8c9b056e17e2b2dff673ab342195901e84
rfnshare <rfnshare@gmail.com>
2021-12-12 13:49:10 +0600

    Variable Declaration

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int myNumber = 5;
+        String myString = "Abdullah Al Faroque";
+        char letter = 'r';
+        float dec = (float) 5.99;
+        double d = 5.99;
+        boolean myCard = true;
+        System.out.println(myNumber + " " + myString + " " + letter + " " + dec + " " + d + " " + myCard);
+
+    }
+}
