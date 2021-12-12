3b02f05b2e9aaee524d2823968fa118848ef6f6c
rfnshare <rfnshare@gmail.com>
2021-12-12 16:16:23 +0600

    ArrayList

--- src/CoreJavaTwo.java
+++ src/CoreJavaTwo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CoreJavaTwo {
     public static void main(String[] args) {
         int[] arr2 = {12, 23, 34, 45, 50};
@@ -15,5 +17,16 @@ public class CoreJavaTwo {
                 }
             }
         }
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("raju");
+        a.add("rashed");
+        a.add("fatema");
+//        a.remove(0);
+        System.out.println(a.get(0));
+//        for(String i: a)
+//        {
+//            System.out.println(a.get(i));
+//        }
+
     }
 }
